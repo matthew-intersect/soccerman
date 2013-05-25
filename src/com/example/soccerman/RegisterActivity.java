9ab8b9d7e1f4de3a2ce4261a44b6470e266995e2
matthew-intersect <matthew@intersect.org.au>
2013-05-25 19:37:52 +1000

    add validation to register screen

--- src/com/example/soccerman/RegisterActivity.java
+++ src/com/example/soccerman/RegisterActivity.java
@@ -24,6 +24,7 @@ public class RegisterActivity extends Activity {
      
     // JSON Response node names
     private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
     private static String KEY_NAME = "name";
     private static String KEY_EMAIL = "email";
     private static String KEY_ID = "id";
@@ -45,36 +46,65 @@ public class RegisterActivity extends Activity {
         // Register Button Click event
         btnRegister.setOnClickListener(new View.OnClickListener() {         
             public void onClick(View view) {
-                String name = inputFullName.getText().toString();
-                String email = inputEmail.getText().toString();
-                String password = inputPassword.getText().toString();
+                String name = inputFullName.getText().toString().trim();
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                
+                StringBuilder errors = new StringBuilder();
+                if(name.equals(""))
+                {
+                	errors.append("Name cannot be blank\n");
+                	inputFullName.setText("");
+                }
+                if(email.equals(""))
+                {
+                	errors.append("Email cannot be blank\n");
+                	inputEmail.setText("");
+                }
+                if(password.equals(""))
+                {
+                	errors.append("Password cannot be blank\n");
+                	inputPassword.setText("");
+                }
+                
+                if(errors.toString() != "")
+                {
+                	registerErrorMsg.setText(errors.toString());
+                	return;
+                }
+                
                 UserFunctions userFunction = new UserFunctions();
                 JSONObject json = userFunction.registerUser(name, email, password);
                  
                 // check for login response
                 try {
-                    if (json.getString(KEY_SUCCESS) != null) {
+                    if (json.getString(KEY_SUCCESS) != null) 
+                    {
                         registerErrorMsg.setText("");
                         String res = json.getString(KEY_SUCCESS); 
-                        if(Integer.parseInt(res) == 1){
+                        if(Integer.parseInt(res) == 1)
+                        {
                             // user successfully registered
-                            // Store user details in SQLite Database
+                            // Store user details in local database 
                             DatabaseHandler db = new DatabaseHandler(getApplicationContext());
                             JSONObject json_user = json.getJSONObject("user");
                              
                             // Clear all previous data in database
                             userFunction.logoutUser(getApplicationContext());
                             db.addUser(json.getInt(KEY_ID), json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json_user.getString(KEY_CREATED_AT));                        
-                            // Launch Dashboard Screen
                             Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
-                            // Close all views before launching Dashboard
                             dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                             startActivity(dashboard);
-                            // Close Registration Screen
                             finish();
-                        }else{
-                            // Error in registration
-                            registerErrorMsg.setText("Error occurred in registration");
+                        }
+                        else
+                        {
+                        	if(Integer.parseInt(json.getString(KEY_ERROR)) == 2)
+                        	{
+                        		registerErrorMsg.setText("User with given email already exists");
+                        		return;
+                        	}
+                            registerErrorMsg.setText("Error occurred in registration. Try again later");
                         }
                     }
                 } catch (JSONException e) {
