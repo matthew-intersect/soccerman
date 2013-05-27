c3bfbd91573592fbcb620457374b265e6e54f6d2
matthew-intersect <matthew@intersect.org.au>
2013-05-27 20:35:02 +1000

    add manager's ability to edit any team player's attendance

--- src/com/example/soccerman/ViewAttendanceListActivity.java
+++ src/com/example/soccerman/ViewAttendanceListActivity.java
@@ -2,18 +2,31 @@ package com.example.soccerman;
 
 import java.util.ArrayList;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import library.MatchFunctions;
+import library.TeamFunctions;
+import library.UserFunctions;
 import models.PlayerAttendance;
 
 import adapters.PlayerAttendanceAdapter;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 @SuppressLint("HandlerLeak")
 public class ViewAttendanceListActivity extends ListActivity
@@ -24,6 +37,8 @@ public class ViewAttendanceListActivity extends ListActivity
 	private PlayerAttendanceAdapter playerAttendanceAdapter;
 	private Button btnBack;
 	private String teamHomeGround;
+	
+	private static String KEY_SUCCESS = "success";
 	
 	public void onCreate(Bundle savedInstanceState) 
 	{
@@ -61,6 +76,120 @@ public class ViewAttendanceListActivity extends ListActivity
         
         Thread thread =  new Thread(null, viewParts, "MagentoBackground");
         thread.start();
+	}
+	
+	public void onListItemClick(ListView l, View v, int pos, long id)
+	{
+		l.showContextMenuForChild(v);
+	}
+    
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+	{
+	    super.onCreateContextMenu(menu, v, menuInfo);
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.view_attendance_menu, menu);
+	    
+	    TeamFunctions teamFunctions = new TeamFunctions();
+	    UserFunctions userFunctions = new UserFunctions();
+	    JSONObject json = teamFunctions.getTeamManager(teamId);
+	    
+	    try
+		{
+			int manager = json.getInt("manager");
+			if(manager != Integer.parseInt(userFunctions.getLoggedInUserId(getApplicationContext())))
+			{
+				menu.getItem(0).setVisible(false);
+			}
+		} 
+	    catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) 
+	{
+		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		switch (item.getItemId()) 
+		{
+			case R.id.set_attendance:
+				final Dialog dialog = new Dialog(ViewAttendanceListActivity.this);
+				dialog.setTitle("Is this player playing?");
+				dialog.setContentView(R.layout.change_attendance);
+				
+				final MatchFunctions matchFunctions = new MatchFunctions();
+				Button btnYes = (Button) dialog.findViewById(R.id.attend_yes);
+				Button btnNo = (Button) dialog.findViewById(R.id.attend_no);
+				final int playerId = playerAttendances.get(info.position).getPlayerId();
+				
+				btnYes.setOnClickListener(new View.OnClickListener()
+				{
+					@Override
+					public void onClick(View v)
+					{
+						JSONObject json = matchFunctions.addAttendance(String.valueOf(playerId), matchId, 1); // 1 for yes
+						try
+	                	{
+	                		if (json.getString(KEY_SUCCESS) != null) 
+	                		{
+	                			if(Integer.parseInt(json.getString(KEY_SUCCESS)) == 1)
+	                			{
+	                				Toast.makeText(ViewAttendanceListActivity.this, "Attendance recorded successfully", Toast.LENGTH_LONG).show();
+	            					dialog.dismiss();
+	            					finish();
+	            					startActivity(getIntent());
+	                			}
+	                			else
+	                			{
+	                				Toast.makeText(ViewAttendanceListActivity.this, "Error occurred. Please try again later", Toast.LENGTH_LONG).show();
+	            					dialog.dismiss();
+	                			}
+	                		}
+	                	}
+						catch (JSONException e) 
+	                	{
+	            			e.printStackTrace();
+	            		}
+					}
+				});
+				
+				btnNo.setOnClickListener(new View.OnClickListener()
+				{
+					@Override
+					public void onClick(View v)
+					{
+						JSONObject json = matchFunctions.addAttendance(String.valueOf(playerId), matchId, 0); // 0 for no
+						try
+	                	{
+	                		if (json.getString(KEY_SUCCESS) != null) 
+	                		{
+	                			if(Integer.parseInt(json.getString(KEY_SUCCESS)) == 1)
+	                			{
+	                				Toast.makeText(ViewAttendanceListActivity.this, "Attendance recorded successfully", Toast.LENGTH_LONG).show();
+	            					dialog.dismiss();
+	            					finish();
+	            					startActivity(getIntent());
+	                			}
+	                			else
+	                			{
+	                				Toast.makeText(ViewAttendanceListActivity.this, "Error occurred. Please try again later", Toast.LENGTH_LONG).show();
+	            					dialog.dismiss();
+	                			}
+	                		}
+	                	}
+						catch (JSONException e) 
+	                	{
+	            			e.printStackTrace();
+	            		}
+					}
+				});
+				
+				dialog.show();
+				return true;
+	    }
+	    return false;
 	}
 	
 	private Handler handler = new Handler()
